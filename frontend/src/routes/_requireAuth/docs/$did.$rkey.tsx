9b00a7ee07ef0e0540d48bc1d5ec2e2cdcb8bf16
Sashank Gogula <sashankg@gmail.com>
2025-11-02 21:48:17 -0800

    switch docs app to privi endpoints

--- frontend/src/routes/_requireAuth/docs/$did.$rkey.tsx
+++ frontend/src/routes/_requireAuth/docs/$did.$rkey.tsx
@@ -1,66 +1,68 @@
 import { Editor, EditorContent, useEditor } from "@tiptap/react";
 import { createFileRoute } from "@tanstack/react-router";
-import StarterKit from '@tiptap/starter-kit'
+import StarterKit from "@tiptap/starter-kit";
 import { useMemo } from "react";
 import { useMutation } from "@tanstack/react-query";
 
-export const Route = createFileRoute('/_requireAuth/docs/$did/$rkey')({
+export const Route = createFileRoute("/_requireAuth/docs/$did/$rkey")({
   async loader({ context, params }) {
-    const { did, rkey } = params
-    const response = await context.authSession?.fetchHandler(`/xrpc/com.atproto.repo.getRecord?repo=${did}&collection=com.habitat.docs&rkey=${rkey}`)
+    const { did, rkey } = params;
+    const response = await context.authManager.fetch(
+      `/xrpc/com.habitat.repo.getRecord?repo=${did}&collection=com.habitat.docs&rkey=${rkey}`,
+    );
 
     const data: {
-      uri: string,
-      cid: string,
-      value: HabitatDoc
-    } = await response?.json()
+      uri: string;
+      cid: string;
+      value: HabitatDoc;
+    } = await response?.json();
 
     return {
       ...data,
-    }
+    };
   },
   component() {
-    const { did, rkey } = Route.useParams()
-    const { value } = Route.useLoaderData()
-    const { authSession } = Route.useRouteContext()
+    const { did, rkey } = Route.useParams();
+    const { value } = Route.useLoaderData();
+    const { authManager } = Route.useRouteContext();
     const { mutate: save } = useMutation({
       mutationFn: async ({ editor }: { editor: Editor }) => {
-        const heading = editor.$node('heading')?.textContent
-        authSession?.fetchHandler('/xrpc/com.atproto.repo.putRecord', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
+        const heading = editor.$node("heading")?.textContent;
+        authManager.fetch(
+          "/xrpc/com.habitat.repo.putRecord",
+          "POST",
+          JSON.stringify({
             repo: did,
-            collection: 'com.habitat.docs',
+            collection: "com.habitat.docs",
             rkey,
             record: {
-              name: heading ?? 'Untitled',
-              blob: editor.getHTML()
-            }
-          })
-        })
-      }
-    })
+              name: heading ?? "Untitled",
+              blob: editor.getHTML(),
+            },
+          }),
+        );
+      },
+    });
     // debounce
     const handleUpdate = useMemo(() => {
-      let prevTimeout: number | undefined
+      let prevTimeout: number | undefined;
       return ({ editor }: { editor: Editor }) => {
-        clearTimeout(prevTimeout)
+        clearTimeout(prevTimeout);
         prevTimeout = setTimeout(() => {
-          save({ editor })
-        }, 1000)
-      }
-    }, [save])
+          save({ editor });
+        }, 1000);
+      };
+    }, [save]);
     const editor = useEditor({
       extensions: [StarterKit],
-      content: value.blob || '',
+      content: value.blob || "",
       onUpdate: handleUpdate,
-      editable: did === authSession?.did
-    })
-    return <article>
-      <EditorContent editor={editor} />
-    </article>
-  }
+      editable: did === authManager.handle,
+    });
+    return (
+      <article>
+        <EditorContent editor={editor} />
+      </article>
+    );
+  },
 });
--- frontend/src/routes/_requireAuth/docs/index.tsx
+++ frontend/src/routes/_requireAuth/docs/index.tsx
@@ -1,69 +1,85 @@
-import { createFileRoute, Link, useRouter, } from "@tanstack/react-router";
+import { createFileRoute, Link, useRouter } from "@tanstack/react-router";
 import { useForm } from "react-hook-form";
 
-export const Route = createFileRoute('/_requireAuth/docs/')({
+export const Route = createFileRoute("/_requireAuth/docs/")({
   async loader({ context }) {
-    const did = context.authSession?.did
-    const response = await context.authSession?.fetchHandler(`/xrpc/com.atproto.repo.listRecords?repo=${did}&collection=com.habitat.docs`)
+    const did = context.authManager.handle;
+    const response = await context.authManager.fetch(
+      `/xrpc/com.habitat.repo.listRecords?repo=${did}&collection=com.habitat.docs`,
+    );
     const data: {
       records: {
-        uri: string,
-        cid: string,
-        value: HabitatDoc
-      }[]
-    } = await response?.json()
-    return data
+        uri: string;
+        cid: string;
+        value: HabitatDoc;
+      }[];
+    } = await response?.json();
+    return data;
   },
   staleTime: 1000 * 60 * 60,
   component() {
-    const router = useRouter()
-    const { records } = Route.useLoaderData()
-    const { authSession } = Route.useRouteContext()
-    const navigate = Route.useNavigate()
-    const form = useForm()
-    return <>
-      <form onSubmit={form.handleSubmit(async () => {
-        const did = authSession?.did
-        const response = await authSession?.fetchHandler(`/xrpc/com.atproto.repo.createRecord?repo=${did}`, {
-          method: 'POST',
-          body: JSON.stringify({
-            repo: did,
-            collection: 'com.habitat.docs',
-            record: {
-              name: 'Untitled',
-              blob: null
-            }
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-        const { uri } = await response?.json()
-        navigate({
-          to: '/docs/$did/$rkey',
-          params: { did: authSession?.did ?? '', rkey: uri.split('/').at(-1) ?? '' }
-        })
-        router.invalidate({ filter: (x) => (x.pathname === '/docs/') })
-      })}>
-        <button type="submit" aria-busy={form.formState.isSubmitting}>New</button>
-      </form>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map(doc => (
-            <tr key={doc.cid}>
-              <td>
-                <Link to="/docs/$did/$rkey" params={{ did: authSession!.did, rkey: doc.uri.split('/').at(-1) ?? '' }} >
-                  {doc.value.name || doc.uri}
-                </Link>
-              </td>
+    const router = useRouter();
+    const { records } = Route.useLoaderData();
+    const { authManager } = Route.useRouteContext();
+    const navigate = Route.useNavigate();
+    const form = useForm();
+    return (
+      <>
+        <form
+          onSubmit={form.handleSubmit(async () => {
+            const did = authManager.handle;
+            const response = await authManager.fetch(
+              `/xrpc/com.habitat.repo.createRecord?repo=${did}`,
+              "POST",
+              JSON.stringify({
+                repo: did,
+                collection: "com.habitat.docs",
+                record: {
+                  name: "Untitled",
+                  blob: null,
+                },
+              }),
+            );
+            const { uri } = await response?.json();
+            navigate({
+              to: "/docs/$did/$rkey",
+              params: {
+                did: authManager.handle ?? "",
+                rkey: uri.split("/").at(-1) ?? "",
+              },
+            });
+            router.invalidate({ filter: (x) => x.pathname === "/docs/" });
+          })}
+        >
+          <button type="submit" aria-busy={form.formState.isSubmitting}>
+            New
+          </button>
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
             </tr>
-          ))}
-        </tbody>
-      </table></>
-  }
-})
+          </thead>
+          <tbody>
+            {records.map((doc) => (
+              <tr key={doc.cid}>
+                <td>
+                  <Link
+                    to="/docs/$did/$rkey"
+                    params={{
+                      did: authManager.handle ?? "",
+                      rkey: doc.uri.split("/").at(-1) ?? "",
+                    }}
+                  >
+                    {doc.value.name || doc.uri}
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  },
+});
