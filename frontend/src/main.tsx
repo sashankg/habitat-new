46c9bcd60c467cb0e397fe61e1953a2cf704d9c7
Sashank Gogula <sashank.gogula@glean.com>
2025-11-05 00:53:34 -0800

    point to render

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthManager } from "./auth.ts";
 
-const authManager = new AuthManager("privi.dwelf-mirzam.ts.net", () => {
+const authManager = new AuthManager("habitat-new.onrender.com", () => {
   router.navigate({ to: "/oauth-login" });
 });
 const queryClient = new QueryClient();
